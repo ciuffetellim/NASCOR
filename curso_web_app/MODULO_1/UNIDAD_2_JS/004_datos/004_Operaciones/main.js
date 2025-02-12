25ae39b3cb14d418fbb60f2914d8e85e93666809
ciuffetellim <ciuffetellimassimo@gmail.com>
2025-02-12 10:10:30 +0100

    12Feb_1

--- curso_web_app/MODULO_1/UNIDAD_2_JS/004_datos/004_Operaciones/main.js
+++ curso_web_app/MODULO_1/UNIDAD_2_JS/004_datos/004_Operaciones/main.js
@@ -1,8 +1,8 @@
 let num1 = parseInt(prompt("Introduce un número"));
-document.write('Número introducido: ' + num1);
+document.write('Primer numero: ' + num1);
 
 let num2 = parseInt(prompt("Introduce otro número"));
-document.write('<br>Segundo número introducido: ' + num2);
+document.write('<br>Segundo número: ' + num2);
 
 let suma = num1 + num2;
-document.write('<br><b><i><span id="textos">Suma: ' + suma + '</span> Otra cosa');
+document.write('<br><b><i><span id="textos">Suma: ' + suma + '</span> ');
